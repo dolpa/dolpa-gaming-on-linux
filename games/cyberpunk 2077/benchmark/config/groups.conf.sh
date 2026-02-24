828cb884a2d7672cf3d2309f4e380d93d6c2ccbc
dolpa <42228880+dolpa@users.noreply.github.com>
2026-02-24 20:53:51 +0200

    new groups

--- games/cyberpunk 2077/benchmark/config/groups.conf.sh
+++ games/cyberpunk 2077/benchmark/config/groups.conf.sh
@@ -9,3 +9,7 @@ TEST_GROUPS["fsr-comparison"]="fsr2-quality-1440p-high-rt-off fsr21-quality-1440
 TEST_GROUPS["rt-comparison"]="native-1440p-high-rt-off native-1440p-high-rt-on dlss-quality-1440p-high-rt-on"
 TEST_GROUPS["4k-performance"]="native-4k-high-rt-off dlss-quality-4k-high-rt-off dlss-performance-4k-high-rt-off fsr3-quality-4k-high-rt-off"
 TEST_GROUPS["quick-4k"]="native-4k-high-rt-off dlss-quality-4k-high-rt-off dlss-performance-4k-high-rt-off fsr3-quality-4k-high-rt-off fsr3-performance-4k-high-rt-off dlss-quality-4k-high-rt-on"
+TEST_GROUPS["4k-quick-low"]="native-4k-low-rt-off dlss-ultra-performance-4k-low-rt-off fsr2-performance-4k-low-rt-off fsr3-performance-4k-low-rt-off"
+TEST_GROUPS["4k-quick-medium"]="native-4k-medium-rt-off dlss-ultra-performance-4k-medium-rt-off fsr2-performance-4k-medium-rt-off fsr3-performance-4k-medium-rt-off"
+TEST_GROUPS["4k-quick-high"]="native-4k-high-rt-off dlss-quality-4k-high-rt-off dlss-performance-4k-high-rt-off fsr2-performance-4k-high-rt-off fsr3-performance-4k-high-rt-off"
+TEST_GROUPS["4k-quick-ultra"]="native-4k-ultra-rt-off dlss-quality-4k-ultra-rt-off dlss-ultra-performance-4k-ultra-rt-off fsr2-performance-4k-ultra-rt-off fsr3-performance-4k-ultra-rt-off"
