deea06f7dcf2ef5f7023ba6c6214f0325a825ff6
dolpa <42228880+dolpa@users.noreply.github.com>
2026-03-10 21:25:12 +0200

    group and tests template

--- games/ac valhalla/benchmark/config/groups.conf.sh
+++ games/ac valhalla/benchmark/config/groups.conf.sh
@@ -2,14 +2,10 @@
 # This file is sourced by run_cp2077_benchmark.sh
 # Required by caller: declare -A TEST_GROUPS
 
-TEST_GROUPS["quick"]="native-1080p-high-rt-off dlss-quality-1440p-high-rt-off fsr2-quality-1440p-high-rt-off"
+TEST_GROUPS["native-quick"]="native-1080p-high-rt-off native-1440p-high-rt-off native-4k-high-rt-off"
 TEST_GROUPS["native-comparison"]="native-1080p-high-rt-off native-1440p-high-rt-off native-4k-high-rt-off"
-TEST_GROUPS["dlss-comparison"]="dlss-quality-1440p-high-rt-off dlss-balanced-1440p-high-rt-off dlss-performance-1440p-high-rt-off"
-TEST_GROUPS["fsr-comparison"]="fsr2-quality-1440p-high-rt-off fsr21-quality-1440p-high-rt-off fsr3-quality-1440p-high-rt-off"
-TEST_GROUPS["rt-comparison"]="native-1440p-high-rt-off native-1440p-high-rt-on dlss-quality-1440p-high-rt-on"
-TEST_GROUPS["4k-performance"]="native-4k-high-rt-off dlss-quality-4k-high-rt-off dlss-performance-4k-high-rt-off fsr3-quality-4k-high-rt-off"
-TEST_GROUPS["quick-4k"]="native-4k-high-rt-off native-4k-high-rt-on dlss-quality-4k-high-rt-off dlss-quality-4k-high-rt-on dlss-performance-4k-high-rt-off fsr3-quality-4k-high-rt-off fsr3-performance-4k-high-rt-off fsr3-performance-4k-high-rt-on"
-TEST_GROUPS["4k-quick-low"]="native-4k-low-rt-off native-4k-low-rt-on dlss-ultra-performance-4k-low-rt-off dlss-ultra-performance-4k-low-rt-on fsr2-performance-4k-low-rt-off fsr2-performance-4k-low-rt-on fsr3-performance-4k-low-rt-off fsr3-performance-4k-low-rt-on"
-TEST_GROUPS["4k-quick-medium"]="native-4k-medium-rt-off native-4k-medium-rt-on dlss-ultra-performance-4k-medium-rt-off dlss-ultra-performance-4k-medium-rt-on fsr2-performance-4k-medium-rt-off fsr2-performance-4k-medium-rt-on fsr3-performance-4k-medium-rt-off fsr3-performance-4k-medium-rt-on"
-TEST_GROUPS["4k-quick-high"]="native-4k-high-rt-off native-4k-high-rt-on dlss-quality-4k-high-rt-off dlss-quality-4k-high-rt-on dlss-performance-4k-high-rt-off fsr2-performance-4k-high-rt-off fsr2-performance-4k-high-rt-on fsr3-performance-4k-high-rt-off fsr3-performance-4k-high-rt-on"
-TEST_GROUPS["4k-quick-ultra"]="native-4k-ultra-rt-off native-4k-ultra-rt-on dlss-quality-4k-ultra-rt-off dlss-quality-4k-ultra-rt-on dlss-ultra-performance-4k-ultra-rt-off dlss-ultra-performance-4k-ultra-rt-on fsr2-performance-4k-ultra-rt-off fsr2-performance-4k-ultra-rt-on fsr3-performance-4k-ultra-rt-off fsr3-performance-4k-ultra-rt-on"
+TEST_GROUPS["native-1080p-scaling"]="native-1080p-low-rt-off native-1080p-medium-rt-off native-1080p-high-rt-off native-1080p-ultra-rt-off"
+TEST_GROUPS["native-1440p-scaling"]="native-1440p-low-rt-off native-1440p-medium-rt-off native-1440p-high-rt-off native-1440p-ultra-rt-off"
+TEST_GROUPS["native-4k-scaling"]="native-4k-low-rt-off native-4k-medium-rt-off native-4k-high-rt-off native-4k-ultra-rt-off"
+TEST_GROUPS["all-native"]="native-1080p-low-rt-off native-1080p-medium-rt-off native-1080p-high-rt-off native-1080p-ultra-rt-off native-1440p-low-rt-off native-1440p-medium-rt-off native-1440p-high-rt-off native-1440p-ultra-rt-off native-4k-low-rt-off native-4k-medium-rt-off native-4k-high-rt-off native-4k-ultra-rt-off"
+
