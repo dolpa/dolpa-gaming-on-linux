c575e26b129e92e6f7f4f86d3c555620af3bfe16
dolpa <42228880+dolpa@users.noreply.github.com>
2026-03-10 21:25:56 +0200

    global benchmark configuration

---
+++ games/etc/benchmark.config.sh
@@ -0,0 +1,9 @@
+
+# Global Resolution configurations for benchmarks
+declare -A GAME_RESOLUTIONS
+
+GAME_RESOLUTIONS=(
+    ["1080p"]="1920x1080"
+    ["1440p"]="2560x1440"
+    ["2160p"]="3840x2160"
+)
