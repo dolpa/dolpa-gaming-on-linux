29ed620dd6fd10debf1fb53232db7620db9e5f92
dolpa <42228880+dolpa@users.noreply.github.com>
2026-03-10 21:24:49 +0200

    ac valhalla game global settings

---
+++ games/ac valhalla/benchmark/config/game.ac-valhalla.conf.sh
@@ -0,0 +1,12 @@
+
+
+# Game/Steam identifiers
+GAME_ID=2208920                             # Steam AppID for Assassin’s Creed Valhalla
+GAME_NAME="Assassin’s Creed Valhalla"       # Human‑friendly game name (used for logging, folder names, …)
+GANME_EXE_PATH=""                           # Relative path to the game executable from the game root folder (used to locate the executable for launching)
+GAME_EXE="ACValhalla.exe"                   # Game executable name (used to locate the executable for launching)
+GAME_CREATOR_STUDIO="Ubisoft"               # Creator studio name (used for folder paths in the user settings and benchmark results)
+GAME_LAUNCH_ARGS="--launcher-skip --intro-skip -benchmark"
+                                            # Game launcher name (used for folder paths in the user settings and benchmark results)
+
+GAME_PROFILE_EXTENSION="ini"
